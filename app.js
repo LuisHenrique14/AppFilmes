ea42330b66ef157bb32fe9ca49229227e701dc5c
luishenrique <lhmnunes@outlook.com>
2022-12-26 19:45:40 -0300

    upgrade

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ let filmes = []
 btn1.onclick = function(){
     let textElement = '';
 
-    let inputValue = document.querySelector('input').value;
+    let inputValue = document.querySelector('input[name="nome"]').value;
 
     let pElement = document.createElement('h6');
     
@@ -27,7 +27,65 @@ btn1.onclick = function(){
 
     divElement.appendChild(pElement);
 
-    document.querySelector('input').value = "";    
+    document.querySelector('input').value = "";
+    
+    div.innerHTML = ''
+
+    let spanTitulo = document.createElement('li')
+    let spanAno = document.createElement('li')
+    let spanGenero = document.createElement('li')
+    let spanDiretor = document.createElement('li')
+    let spanAtores = document.createElement('li')
+    let spanResumo = document.createElement('li')
+
+
+    let txtTitulo = ''
+    let txtAno = ''
+    let txtGenero = ''
+    let txtDiretor = ''
+    let txtAtores = ''
+    let txtResumo = ''
+    
+
+    let filme = document.querySelector('input[name=filme_user]').value
+    
+
+    axios.get(`https://www.omdbapi.com/?t=${inputValue}&apikey=9d5444c3`)
+        .then(function(response){
+            if(response.data.logradouro !== null){
+                txtTitulo = document.createTextNode('Título: '+ response.data.Title)
+                txtAno = document.createTextNode('Ano: ' + response.data.Year)
+                txtGenero = document.createTextNode('Genero: ' + response.data.Genre)
+                txtDiretor = document.createTextNode('Diretor: ' + response.data.Director)
+                txtAtores = document.createTextNode('Atores: ' + response.data.Actors)
+                txtResumo = document.createTextNode('Sinopse: ' + response.data.Plot)
+                let img = document.createElement('img')
+                img.setAttribute('src', response.data.Poster)
+                img.setAttribute('alt', response.data.Title)
+                img.setAttribute('width', '200px')
+                img.setAttribute('height', '300px')
+                div.appendChild(img)
+                
+            }else{
+                txtTitulo = document.createTextNode('O usuário não possui nome')
+            }
+            spanTitulo.appendChild(txtTitulo)
+            spanAno.appendChild(txtAno)
+            spanGenero.appendChild(txtGenero)
+            spanDiretor.appendChild(txtDiretor)
+            spanAtores.appendChild(txtAtores)
+            spanResumo.appendChild(txtResumo)
+            div.appendChild(spanTitulo)
+            div.appendChild(spanAno)
+            div.appendChild(spanGenero)
+            div.appendChild(spanDiretor)
+            div.appendChild(spanAtores)
+            div.appendChild(spanResumo)
+
+            filme.innerHTML = ''
+        })
+
+    axios.get(`http://img.omdbapi.com/?t=${filme}apikey=9d5444c3&`)
 
 }
 
@@ -38,6 +96,7 @@ btn2.onclick = function(){
     spans.innerHTML = filmes[numero]
 }
 
+let btn3 = document.querySelector('#btn3')
 
-
+let div = document.querySelector('#app1')
 
